e462abbed491710091806a924b08c92f2c3ea1ce
root <root@PC-Kathe>
2024-02-11 14:07:00 -0500

    update github token

--- github.tf
+++ github.tf
@@ -9,7 +9,7 @@ terraform {
 
 # Configure the GitHub Provider
 provider "github" {
-  token = "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"
+  token = "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"
 }
 
 resource "github_repository" "example" {
