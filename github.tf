4f0a8137f374f20afb60533a932529ea09722bdb
root <root@PC-Kathe>
2024-02-11 14:14:43 -0500

    Update workflow

--- github.tf
+++ github.tf
@@ -9,7 +9,7 @@ terraform {
 
 # Configure the GitHub Provider
 provider "github" {
-  token = "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"
+  token = "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"
 }
 
 resource "github_repository" "example" {
