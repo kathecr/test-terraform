d9fc7345be0591a14e5697f422003784bddbe400
Katherine Castelblanco <72935783+kathecr@users.noreply.github.com>
2024-02-14 00:12:43 -0500

    Update github.tf

--- github.tf
+++ github.tf
@@ -2,14 +2,13 @@ terraform {
   required_providers {
     github = {
       source  = "integrations/github"
-      version = "~> 5.0"
+      version = "6.0.0-rc2"
     }
   }
 }
 
 # Configure the GitHub Provider
 provider "github" {
-  token = "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"
 }
 
 resource "github_repository" "example" {
