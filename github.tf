504a7f5f1f48d743edf5f856beed063ad8c50dab
Katherine Castelblanco <72935783+kathecr@users.noreply.github.com>
2024-02-13 23:29:54 -0500

    Update github.tf

--- github.tf
+++ github.tf
@@ -9,7 +9,7 @@ terraform {
 
 # Configure the GitHub Provider
 provider "github" {
-  token = "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"
+  token = "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"
 }
 
 resource "github_repository" "example" {
